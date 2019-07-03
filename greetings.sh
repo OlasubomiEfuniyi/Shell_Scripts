a2d6b2993d25a24adfcba228a3fff7efbbcfd791
Olasubomi Efuniyi <subsefun@gmail.com>
2019-07-03 13:45:15 -0500

    Fixed elif space mistake in greetings.sh

--- greetings.sh
+++ greetings.sh
@@ -6,7 +6,7 @@ time_of_day_greeting() {
 
 	if [ "$hour" -ge "18" ]; then
 		echo "Good evening"
-	elif ["$hour" -ge "12" ]; then
+	elif [ "$hour" -ge "12" ]; then
 		echo "Good afternoon"
 	else 
 		echo "Good morning"
